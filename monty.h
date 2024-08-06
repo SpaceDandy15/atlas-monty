de5027b97f73766286aac1bee5adf2b4c7be042f
Malik Vance <malik@atlassian.com>
2024-08-05 22:06:02 -0500

    fix in main

--- monty.h
+++ monty.h
@@ -52,7 +52,7 @@ void global_free(void);
 
 void push(stack_t **stack, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
-void _pint(stack_t **stack, unsigned int line_number)
+void _pint(stack_t **stack, unsigned int line_number);
 void _swap(stack_t **stack, unsigned int line_number);
 void _pop(stack_t **stack, unsigned int line_number);
 void _add(stack_t **stack, unsigned int line_number);
