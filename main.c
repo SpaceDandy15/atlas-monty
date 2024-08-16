71f2ec69330741edb8574f3db927b1117df269eb
Malik Vance <malik@atlassian.com>
2024-08-15 21:16:25 -0500

    fixing betty

--- main.c
+++ main.c
@@ -7,15 +7,12 @@ instruction_t instructions[] = {
 	{NULL, NULL}
 };
 
+void process_file(FILE *file);
+void execute_instruction(char *opcode, stack_t **stack, unsigned int line_number);
+
 int main(int argc, char **argv)
 {
 	FILE *file;
-	char *line = NULL;
-	size_t len = 0;
-	unsigned int line_number = 0;
-	stack_t *stack = NULL;
-	char *opcode;
-	instruction_t *instruction;
 
 	if (argc != 2)
 	{
@@ -30,37 +27,53 @@ int main(int argc, char **argv)
 		exit(EXIT_FAILURE);
 	}
 
+	process_file(file);
+	
+	flcose(file);
+	return (EXIT_SUCCESS);
+}
+
+void process_file(FILE *file)
+{
+	char *line = NULL;
+	size_t len = 0;
+	unsigned int line_number = 0;
+	stack_t *stack = NULL;
+
 	while (getline(&line, &len, file) != -1)
 	{
 		line_number++;
-		opcode = strtok(line, " \n\t");
-		if (opcode == NULL || opcode [0] == '#')
+		char *opcode = strtok(line, " \n\t");
+		if (opcode == NULL || opcode[0] == '#')
+		
 		continue;
 
-		for (instruction = instructions; instruction->opcode != NULL; instruction++)
-		{
-			if (strcmp(opcode, instruction->opcode) == 0)
-			{
-				instruction->f(&stack, line_number);
-				break;
-			}
-		}
-
-		if (instruction->opcode == NULL)
-		{
-			fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
-			exit(EXIT_FAILURE);
-		}
+		execute_instruction(opcode, &stack, line_number);
 	}
-	
+
 	free(line);
-	fclose(file);
-	/**free the stack*/
+
 	while (stack != NULL)
 	{
 		stack_t *temp = stack;
 		stack = stack->next;
 		free(temp);
 	}
-	return (EXIT_SUCCESS);
+}
+
+void execute_instruction(char *opcode, stack_t **stack, unsigned int line_number)
+{
+	instruction_t *instruction;
+
+	for (instruction = instructions; instruction->opcode != NULL; instruction++)
+	{
+		if (strcmp(opcode, instruction->opcode) == 0)
+		{
+			instruction->f(stack, line_number);
+			return;
+		}
+	}
+
+	fprintf(stderr, "L%d: unknown instruction %s\n", line_number, opcode);
+	exit(EXIT_FAILURE);
 }
--- push.c
+++ push.c
@@ -18,7 +18,7 @@ void push(stack_t **stack, unsigned int line_number)
 	if (new_node == NULL)
 	{
 		fprintf(stderr, "Error: malloc failed\n");
-		exit (EXIT_FAILURE);
+		exit(EXIT_FAILURE);
 	}
 	new_node->n = value;
 	new_node->prev = NULL;
