8160865af0505267169cd9093347e5f611b9d1ed
Malik Vance <malik@atlassian.com>
2024-08-15 19:37:38 -0500

    fixed files

--- push.c
+++ push.c
@@ -2,44 +2,44 @@
 
 void push(stack_t **stack, unsigned int line_number)
 {
-    char *arg;
-    int value;
-    stack_t *new_node;
+	char *arg;
+	int value;
+	stack_t *new_node;
 
-    arg = strtok(NULL, " \n\t");
-    if (arg == NULL || !is_number(arg))
-    {
-        fprintf(stderr, "L%d: usage: push integer\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-    value = atoi(arg);
+	arg = strtok(NULL, " \n\t");
+	if (arg == NULL || !is_number(arg))
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	value = atoi(arg);
 
-    new_node = malloc(sizeof(stack_t));
-    if (new_node == NULL)
-    {
-        fprintf(stderr, "Error: malloc failed\n");
-        exit(EXIT_FAILURE);
-    }
-    new_node->n = value;
-    new_node->prev = NULL;
-    new_node->next = *stack;
+	new_node = malloc(sizeof(stack_t));
+	if (new_node == NULL)
+	{
+		fprintf(stderr, "Error: malloc failed\n");
+		exit (EXIT_FAILURE);
+	}
+	new_node->n = value;
+	new_node->prev = NULL;
+	new_node->next = *stack;
 
-    if (*stack != NULL)
-        (*stack)->prev = new_node;
-    *stack = new_node;
+	if (*stack != NULL)
+	(*stack)->prev = new_node;
+	*stack = new_node;
 }
 
 int is_number(char *str)
 {
-    int i = 0;
+	int i = 0;
 
-    if (str[0] == '-' || str[0] == '+')
-        i++;
+	if (str[0] == '-' || str[0] == '+')
+	i++;
 
-    for (; str[i]; i++)
-    {
-        if (!isdigit(str[i]))
-            return (0);
-    }
-    return (1);
+	for (; str[i]; i++)
+	{
+		if (!isdigit(str[i]))
+		return (0);
+	}
+	return (1);
 }
