2d7ca2592f9bff8220aab12772e00ee2a0448af7
Malik Vance <malik@atlassian.com>
2024-08-15 20:17:40 -0500

    USAGE

--- main.c
+++ main.c
@@ -19,9 +19,10 @@ int main(int argc, char **argv)
 
 	if (argc != 2)
 	{
-		fprintf(stderr, "Usage: monty file\n");
+		fprintf(stderr, "USAGE: monty file\n");
 		exit(EXIT_FAILURE);
 	}
+
 	file = fopen(argv[1], "r");
 	if (file == NULL)
 	{
